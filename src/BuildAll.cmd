809e2cde7982d98561851c6c37cdacf8af084de3
Nathan Allen-Wagner <nathan@alner.net>
2012-01-30 04:31:21 +0000

    Updating build script to include step for compiling nuget package. Updating version on dlls to 1.0.0.2. Adding NuGet.exe to source tree.
    git-svn-id: https://muf.svn.codeplex.com/svn@13676 37308e9d-ab76-48fb-8b0d-99fc395a6d69

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -21,6 +21,45 @@ REM "==============="
 REM "    PASSED     "
 REM "==============="
 REM "==============="
+
+if not exist "..\nuget" mkdir "..\nuget"
+if not exist "..\nuget\content" mkdir "..\nuget\content"
+if not exist "..\nuget\lib" mkdir "..\nuget\lib"
+if not exist "..\nuget\lib\net35" mkdir "..\nuget\lib\net35"
+if not exist "..\nuget\lib\net40" mkdir "..\nuget\lib\net40"
+if not exist "..\nuget\lib\sl3-wp" mkdir "..\nuget\lib\sl3-wp"
+if not exist "..\nuget\lib\sl40" mkdir "..\nuget\lib\sl40"
+
+copy /y "..\docs\Monitored Undo Documentation.docx" "..\nuget\content\Monitored Undo Documentation.docx"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\NET40\MonitoredUndo.dll" "..\nuget\lib\net40\MonitoredUndo.dll"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\NET40\MonitoredUndo.pdb" "..\nuget\lib\net40\MonitoredUndo.pdb"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\NET35\MonitoredUndo35.dll" "..\nuget\lib\net35\MonitoredUndo35.dll"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\NET35\MonitoredUndo35.pdb" "..\nuget\lib\net35\MonitoredUndo35.pdb"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\SL4\MonitoredUndoSL4.dll" "..\nuget\lib\sl40\MonitoredUndoSL4.dll"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\SL4\MonitoredUndoSL4.pdb" "..\nuget\lib\sl40\MonitoredUndoSL4.pdb"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\WP7\MonitoredUndoWP7.dll" "..\nuget\lib\sl3-wp\MonitoredUndoWP7.dll"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+copy /y "..\releases\Latest\WP7\MonitoredUndoWP7.pdb" "..\nuget\lib\sl3-wp\MonitoredUndoWP7.pdb"
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+..\tools\nuget.exe pack ..\nuget\MUF.1.0.nuspec -OutputDirectory ..\nuget\ -Version 1.1
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
 GOTO DONE
 
 :ERROR
@@ -32,3 +71,4 @@ REM "==============="
 
 :DONE
 PAUSE
+
