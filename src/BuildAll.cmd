453ffaad3f3a54210638230ba81dc0a319d42b34
Nathan Allen-Wagner <nathan@alner.net>
2013-02-06 15:23:44 -0500

    Added unit test to build script.

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -10,13 +10,25 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /target:Rebuild /property:Configuration=Release;OutDir=..\..\..\..\releases\Latest\NET35\ "Net35\MonitoredUndo35\MonitoredUndo35.sln"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-xcopy 
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\MSTest.exe" /testcontainer:"..\releases\Latest\NET40\MonitoredUndoTests.dll" /noresults
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\MSTest.exe" /testcontainer:"..\releases\Latest\NET35\MonitoredUndoTests35.dll" /noresults
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+REM "==============="
+REM "==============="
+REM "    PASSED     "
+REM "==============="
+REM "==============="
 GOTO DONE
 
 :ERROR
-echo "BUILD FAILED"
+REM "==============="
+REM "==============="
+REM "    FAILED     "
+REM "==============="
+REM "==============="
 
 :DONE
 PAUSE
